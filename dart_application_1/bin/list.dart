682948dfed3d899d28cf267a80abb86419edc7e0
Jadev03 <sathiendrathabendra@gmail.com>
2024-06-23 18:50:38 +0530

    commit

---
+++ dart_application_1/bin/list.dart
@@ -0,0 +1,20 @@
+void main(){
+  var items=["ball","bat",true,false,1,1.2]; 
+  print(items);
+  // try{
+  //   items[6]="Dummy";
+  // }
+  // catch(e){
+  //   e.toString();
+  //   print("exception found");
+
+  // }
+  // finally{
+  //   print(items);
+  // }
+  //String type List
+  List<String> names=["thabe", "jathavan","done"];
+  print(names);
+  var num=<int>[1,3,5,2,5];
+  print(num);
+}
---
+++ dart_application_1/bin/variable.dart
@@ -0,0 +1,16 @@
+void main(){
+  var x=10;
+  print(x);
+  var y="eee";
+  print(y);
+  var z=true;
+  print(z);
+  int a=20;
+  print(a);
+  double b=10.34;
+  print(b);
+  String name="Thabendra";
+  print("name is $name");
+  var numb1=2,numb2=3;
+  print(numb1 +numb2);
+}
